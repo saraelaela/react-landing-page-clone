6158a82a799a94346047a0cf5d646b8e6b43213f
saraelaela <saraelabed@outlook.de>
2024-09-24 19:59:17 +0200

    Fixing ESLint issues, font family fallback was missing

--- src/TextInfo.js
+++
@@ -1,8 +0,0 @@
-export default function TextInfo() {
-  return (
-    <section className="Hero">
-      <p>Stay creative with</p>
-      <h1>Inflatable abstract 3d illustrations</h1>
-    </section>
-  );
-}
